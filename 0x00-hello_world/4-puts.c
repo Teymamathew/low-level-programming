dd302409891306842966ba6facc85a110241933b
Ladyroyale <temilayomathew@gmail.com>
2023-05-29 00:10:27 -0700

    hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *  * main - use put function to print
+ *   *
+ *    * Return: 0 alaways when successful
+ *     */
+
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *  * main - print using prinf
+ *   *
+ *    * Return: 0 always when seccessful
+ *     */
+
+int main(void)
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
+}
